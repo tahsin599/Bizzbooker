74e34e1c7fce4f987ebc7a6984a0035423110853
Tahsin <ktahsin280@gmail.com>
2025-07-27 09:39:43 +0600

    deploy

--- Backend/src/main/java/com/tahsin/backend/Controller/AIConversationController.java
+++ Backend/src/main/java/com/tahsin/backend/Controller/AIConversationController.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+
 @RestController
 @RequestMapping("/api/conversations")
 public class AIConversationController {
